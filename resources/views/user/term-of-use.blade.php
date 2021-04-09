c5212992a83bc507df47765c8c87739388ea667e
vovietha <vovietha365@gmail.com>
2021-04-10 03:54:53 +0700

    about and term

--- resources/views/user/term-of-use.blade.php
+++ resources/views/user/term-of-use.blade.php
@@ -3,5 +3,70 @@
 @section('title', 'Điều khoản sử dụng')
 
 @section('content')
-    
+<div class="container py-5">
+    <h2>
+    ĐIỀU KHOẢN VÀ DỊCH VỤ
+    </h2>
+        <h5 id="gt">GIỚI THIỆU</h5>
+            <p>
+            1.1 Chào mừng bạn đến với trang web mua sắm Flipper qua giao diện 
+            website hoặc ứng dụng di động .Trước khi sử dụng Trang  Flipper 
+            hoặc tạo tài khoản  Flipper vui lòng đọc kỹ các Điều Khoản Dịch Vụ dưới đây 
+            và Quy Chế Hoạt Động trên trang web mua sắm Flipper. 
+            Để hiểu rõ quyền lợi và nghĩa vụ hợp pháp của mình đối với  Flipper.
+            “Dịch Vụ” chúng tôi cung cấp hoặc sẵn .
+            </p>
+            <p>
+            1.2 Các dịch vụ được cung cấp bởi trang Flipper 
+            và bởi phần mềm dành cho khách hàng của Flipper sẵn trên trang Flipper,
+            và tất cả các thông tin mua sắm trên trang web Flipper của chúng tôi. 
+            Bất kỳ tính năng mới nào được bổ sung hoặc mở rộng đối với dịch vụ đều 
+            thuộc phạm vi điều chỉnh của điều Khoản dịch vụ này. 
+            Điều Khoản Dịch Vụ này điều chỉnh việc  bạn sử dụng dịch vụ cung cấp bởi Flipper.
+            trở thành người sử dụng của trang Flipper bạn cần đọc và chấp nhận mọi điều khoản và điều kiện 
+            được quy định trong, và dẫn chiếu đến, 
+            điều khoản dịch vụ này và chính sách bảo mật được dẫn chiếu theo đây.
+            </p>
+            <p>
+            1.3 Trước khi trở thành người sử dụng của trang Flipper, bạn cần đọc và chấp nhận mọi điều khoản
+            và điều kiện được quy định trong, và dẫn chiếu đến, 
+            điều Khoản dịch vụ này và chính sách bảo mật được dẫn chiếu theo đây.
+            </p>
+            <p>
+            1.4 Flipper bảo lưu quyền thay đổi, chỉnh sửa, tạm ngưng hoặc chấm dứt tất cả hoặc bất kỳ phần nào 
+            của trang Flipper hoặc dịch vụ vào bất cứ thời điểm nào theo qui định pháp luật. 
+            Phiên Bản thử nghiệm của 
+            Dịch Vụ hoặc tính năng của Dịch Vụ có thể không hoàn toàn giống với phiên bản cuối cùng.
+            </p>
+            <p>
+            1.5 Flipper bảo lưu quyền từ chối yêu cầu mở tài Khoản hoặc các truy cập của bạn tới 
+            trang Flipper hoặc dịch Vụ theo quy định pháp luật và điều khoản dịch vụ.
+            </p>
+        <h5>QUYỀN RIÊNG TƯ</h5>
+            <p>
+            2.1 Flipper coi trọng việc bảo mật thông tin của bạn. Để bảo vệ quyền lợi người dùng, 
+            Flipper cung cấp chính sách bảo mật tại Flipper để giải thích chi tiết các hoạt động bảo mật
+            của Flipper. Vui lòng tham khảo chính sách bảo mật để biết cách thức Flipper 
+            thu thập và sử dụng thông tin liên quan đến Tài Khoản và/hoặc việc sử dụng dịch vụ của 
+            người sử dụng (thông tin người sử dụng). Điều khoản dịch vụ này có liên quan mật thiết 
+            với chính sách bảo mật. Bằng cách sử dụng dịch vụ hoặc cung cấp thông tin trên trang Flipper, 
+            người sử dụng:
+            </p>
+            <ul>
+                <li>Cho phép Flipper thu thập, sử dụng, công bố và/hoặc xử lý các nội dung, dữ liệu cá nhân 
+            của bạn và thông tin người sử dụng như được quy định trong chính sách bảo mật.</li>
+                <li>Sẽ không, dù là trực tiếp hay gián tiếp, tiết lộ các thông tin người sử dụng cho bất kỳ 
+            bên thứ ba nào, hoặc bằng bất kỳ phương thức nào cho phép bất kỳ bên thứ ba nào được truy cập 
+            hoặc sử dụng thông tin người dùng của bạn.</li>
+            </ul>
+            <p>
+            Trường hợp người sử dụng sở hữu dữ liệu cá nhân của người sử dụng khác thông qua việc sử dụng dịch vụ 
+            (bên nhận thông tin) theo đây đồng ý rằng, mình sẽ tuân thủ mọi qui định pháp luật về bảo vệ an toàn 
+            thông tin cá nhân liên quan đến những thông tin đó, cho phép người sử dụng là chủ sở hữu của các thông tin 
+            cá nhân mà bên nhận thông tin thu thập được (bên tiết lộ thông tin) được phép xóa bỏ thông tin của mình 
+            được thu thập từ cơ sở dữ liệu của bên nhận thông tin và cho phép bên tiết lộ thông tin rà soát 
+            những thông tin đã được thu thập về họ bởi bên nhận thông tin, phù hợp với hoặc theo yêu cầu của 
+            các qui định pháp luật hiện hành.
+            </p>
+</div>
 @endsection
