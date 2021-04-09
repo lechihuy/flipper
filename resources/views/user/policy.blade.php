dbdb6f4e224e0ef5fef6a6cec89006189865ad57
vovietha <vovietha365@gmail.com>
2021-04-10 04:54:57 +0700

    chinh sach

--- resources/views/user/policy.blade.php
+++ resources/views/user/policy.blade.php
@@ -3,5 +3,58 @@
 @section('title', 'Chính sách')
 
 @section('content')
-    
+<div class="container py-5">
+    <h2>CHÍNH SÁCH</h2>
+        <h5>ĐẢM BẢO THÔNG TIN CỦA BẠN PHÙ HỢP VÀ CHUYÊN NGHIỆP</h5>
+            <p>
+            Không sử dụng các từ, cụm từ hoặc ký hiệu trong mọi thông tin điện tử – 
+            cả thông tin nội bộ của Flipper hoặc thông tin kinh doanh bên ngoài – mà có thể được nhiều đối 
+            tượng xem là không phù hợp, công kích hoặc phỉ báng. Xem xét cẩn thận nội dung của bất kỳ thông 
+            tin nào trước khi xuất bản hoặc phân phối và xem xét đối tượng dự định và không dự định 
+            (là nội dung có thể được chia sẻ ngoài đối tượng ban đầu). 
+            Xem xét liệu nội dung, nếu được chia sẻ công khai hoặc trát đòi, sẽ gây tổn hại hoặc gây lúng 
+            túng cho bạn hoặc Flipper hay không. Các chính sách chống quấy rối và chống phân biệt đối xử của 
+            Flipper và bất kỳ quy định tương tự nào trong sổ tay, quy tắc làm việc, quy định nội bộ của công ty 
+            hoặc thỏa thuận lao động của bạn được áp dụng cho thông tin kinh doanh nội bộ và bên ngoài bằng văn 
+            bản cũng như bằng lời nói. Theo mọi chính sách hoặc thông báo giám sát địa phương của Flipper và được 
+            cho phép theo luật hiện hành, Flipper có thể xem xét thông tin để xác định xem có bất kỳ chính sách nào 
+            của Flipper bị vi phạm hay không và sẽ thực hiện các biện pháp thích hợp để khắc phục mọi hành vi 
+            không phù hợp.
+            </p>
+        <h5>BLOG NỘI BỘ VÀ THÔNG TIN NỘI BỘ KHÁC</h5>
+            <p>
+            Khi phát triển blog nội bộ hoặc thông tin khác, hãy nhớ rằng tất cả các chính sách của Flipper đều được áp dụng, bao gồm chính sách này và chính sách về vấn đề 
+            tôn trọng. Nếu blog hoặc thông tin khác của bạn chứa các liên kết đến nội dung khác ngoài tầm kiểm 
+            soát của bạn (như trang web bên ngoài), trước tiên hãy chắc chắn xem lại tất cả các liên kết liên quan 
+            đến nội dung để đảm bảo rằng nó phù hợp.
+            </p>
+        <h5>CHÍNH SÁCH TRUYỀN THÔNG XÃ HỘI</h5>
+            <p>
+            Việc sử dụng các nền tảng truyền thông xã hội như Facebook, Twitter, LinkedIn và các nền tảng 
+            khác được đề cập trong Chính Sách của Flipper về truyền thông trên Các trang Web bên ngoài và 
+            các nền tảng truyền thông xã hội.
+            </p>
+        <h5> SỬ DỤNG KHÔNG ĐÚNG CÁCH</h5>
+            <p>
+            Việc sử dụng không đúng cách hệ thống Flipper hoặc tài liệu Flipper, như được nêu trong các chính sách 
+            hiện hành của Flipper hoặc trong hợp đồng lao động, nếu áp dụng, có thể dẫn đến việc giới hạn hoặc thu hồi 
+            các đặc quyền truy cập điện tử của bạn cũng như hành động khắc phục, lên đến và bao gồm cả sa thải. Các hoạt động mà có thể cấu thành việc sử dụng không đúng cách bao gồm, nhưng không giới hạn, những hành động sau đây:
+            gửi tài liệu Flipper cho bất kỳ ai không có quyền biết hoặc sở hữu nó, truy cập Tài Liệu Flipper cho mục đích phi kinh doanh;
+            gửi hoặc truy xuất các tin nhắn, phim hoạt hình hoặc truyện cười khiêu dâm hoặc xúc phạm, gièm pha dân tộc, biệt danh chủng tộc hoặc bất kỳ phát biểu hay hình ảnh nào khác có thể được hiểu là quấy rối, phỉ báng hoặc bôi nhọ bằng cách sử dụng hệ thống Flipper hoặc khi tiến hành kinh doanh Flipper;
+            tiến hành kinh doanh Flipper bên ngoài hệ thống Flipper (ví dụ: với các hệ thống truyền thông điện tử không do Flipper cung cấp hoặc phê duyệt),
+            truy cập tài liệu Flipper thông qua một thiết bị không do Flipper cung cấp, hoặc bên ngoài các phương thức truy cập an toàn do Flipper phê duyệt, bao gồm nhưng không giới hạn: các hệ thống quản lý thiết bị di động, cơ sở hạ tầng máy tính để bàn ảo, các kết nối VPN, hoặc các ứng dụng an toàn do Flipper cung cấp;
+            điều hành kinh doanh cá nhân bằng Hệ Thống Flipper hoặc sử dụng Hệ Thống Flipper hay Tài Liệu Flipper cho các mục đích thương mại khác không liên quan đến kinh doanh của Flipper;
+            sử dụng Hệ Thống Flipper quá mức hoặc không hợp lý, bao gồm cả việc sử dụng gây cản trở hiệu suất làm việc của bạn hoặc những sử dụng nhiều hơn nguồn lực thông thường;
+            gửi các quan điểm cá nhân không được yêu cầu hoặc không mong muốn về các vấn đề xã hội, chính trị, tôn giáo hoặc các vấn đề khác không liên quan đến kinh doanh bằng cách sử dụng Hệ Thống Flipper;
+            tham gia các hoạt động bất hợp pháp, bao gồm gửi hoặc nhận tài liệu có bản quyền theo cách vi phạm luật bản quyền hoặc thỏa thuận cấp phép bằng Hệ Thống Flipper;
+            tự động chuyển tiếp email người dùng cá nhân của Flipper đến bất kỳ địa chỉ bên ngoài nào;
+            tham gia doanh nghiệp cá nhân, các hoạt động kiếm lợi nhuận hoặc các hoạt động cá nhân phát sinh thêm chi phí cho Flipper hoặc gây cản trở hiệu suất làm việc của nhân viên;
+            tạo và cài đặt bất kỳ vi rút, sâu, Trojan Horse hoặc bất kỳ loại tệp điện tử nào khác có thể có khả năng gây ảnh hưởng tai hại đến hệ thống hay người dùng khác hoặc thu thập thông tin cá nhân hoặc doanh nghiệp mà không có sự chấp thuận;
+            gửi thư chuỗi, tệp đính kèm không liên quan đến công việc, các quảng cáo, lời chào và tin nhắn quảng bá (bao gồm các cảnh báo vi-rút máy tính) bằng Hệ Thống Flipper;
+            chia sẻ mật khẩu của bạn đăng nhập vào Hệ Thống Flipper với bất kỳ ai, cả bên ngoài và bên trong đối với Flipper;
+            cho phép ai đó truy cập Hệ Thống Flipper hoặc Tài Liệu Flipper trong khi tận dụng các quyền và sự cho phép của tài khoản Flipper của bạn;
+            cố ý chấp nhận, truyền hoặc phân phối email hàng loạt không được yêu cầu hoặc thu thập địa chỉ email từ Hệ Thống Flipper nhằm mục đích gửi email không được yêu cầu; hoặc
+            bất kỳ vi phạm nào khác về chính sách của Flipper hoặc luật hiện hành.
+            </p>
+</div>
 @endsection
