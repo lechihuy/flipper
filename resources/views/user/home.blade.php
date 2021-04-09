3bd15e50e1d69990cc627fde3ee64999f0c4fd59
vovietha <vovietha365@gmail.com>
2021-04-10 00:59:03 +0700

    add jumbotron

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -10,7 +10,7 @@
             <div class="col">
                 <h1 class="display-4">Flipper</h1>
                 <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                <button type="button" class="btn btn-light">Notify me +</button>            
+                <a href="http://https://www.google.com.vn/?hl=vi" class="btn btn-light">Xem ngay</a>            
             </div>
         <div class="col">
             <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/5d6dcf91-8064-4869-a6c1-4f982adcbe4a/air-max-270-mens-shoe-KkLcGR.png" alt="Girl in a jacket" width="500" height="500">
