e5e58ae8f28668cd00841dd5ca2aac465d116924
OaH <bnhao10062001@gmail.com>
2021-04-10 02:16:43 +0700

    user-login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -6,33 +6,31 @@
     <form class="form__login mx-auto" class="p-4">
         <div class="form__tab row">
             <div class="form__tab-item col-6 text-center">
-            <a class="form_tab-item-active nav-link d-block" href="#">Đăng ký</a>
+            <a class=" nav-link d-block" href="#">Đăng ký</a>
             </div>
             <div class="form__tab-item col-6 text-center">
-            <a class="nav-link d-block" href="#">Đăng nhập</a>
+            <a class="form_tab-item-active nav-link d-block" href="#">Đăng nhập</a>
             </div>
         </div>
 
         <div class="form-group">
-            <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Last Name">
+            <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
         </div>
         <div class="form-group">
-            <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Email Address">
+            <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
         </div>
-        <div class="form-group">
-            <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-        </div>
-
-        <p id="text-codition">At least 8 characters 1 uppercase letter, 1 number & 1 symbol</p>
 
         <div class="form-group">
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="dropdownCheck2">
                 <label id="form-check-text" class="form-check-label" for="dropdownCheck2">
-                    By logging in, you agree to the <b> Terms of Service </b> and <b> Privacy Policy </b> 
+                   Nhớ mật khẩu? 
                 </label>
             </div>
         </div>
-        <button type="submit" class=" form__login-btn btn btn-primary d-block mx-auto btn btn-dark">Login</button>
+        <button type="submit" class=" form__login-btn btn btn-primary d-block mx-auto btn btn-dark">Đăng nhập</button>
+        <div id='form__login-forget' class='text-center'>Quên mật khẩu?</div>
+        <button id='form__login-btn-create' type="submit" class=" form__login-btn btn btn-primary d-block mx-auto btn btn-dark">Tạo tài khoản mới</button>
+        
     </form>
 @endsection
---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,41 @@
+@extends('user.layouts.master')
+
+@section('title', 'Đăng ký')
+
+@section('content')
+    <form class="form__login mx-auto" class="p-4">
+        <div class="form__tab row">
+            <div class="form__tab-item col-6 text-center">
+            <a class="form_tab-item-active nav-link d-block" href="#">Đăng ký</a>
+            </div>
+            <div class="form__tab-item col-6 text-center">
+            <a class="nav-link d-block" href="#">Đăng nhập</a>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Họ và tên của bạn ...">
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Số điện thoại ...">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
+        </div>
+
+        <p id="text-codition">Ít nhất 8 ký tự 1 chữ hoa, 1 số & 1 ký hiệu</p>
+
+        <div class="form-group">
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                <label id="form-check-text" class="form-check-label" for="dropdownCheck2">
+                    Khi đăng nhập, Bạn sẽ phải đồng ý <b> Điều khoản dịch vụ </b> và <b> chính sách quyền riêng tư của cty </b> 
+                </label>
+            </div>
+        </div>
+        <button type="submit" class=" form__login-btn btn btn-primary d-block mx-auto btn btn-dark">Đăng ký</button>
+    </form>
+@endsection
