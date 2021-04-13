cdf33fd7279684f6ad71777dbda6a82a65848c97
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-04-13 13:39:26 +0700

    fix response

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,19 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
-        <img src="{{ asset('images/flipper-light.png') }}" alt="" height="30">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/flipper-light.png') }}" alt="" height="30">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="">Hồ sơ</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin.logout') }}">Thoát</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Hồ sơ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.logout') }}">Thoát</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
