3a50f0fa9fe42d97d27c42438cc03554c42e6909
Son Bill <sonpewds@Asus-Zenbook-13-inch.local>
2021-04-13 21:09:28 +0700

    save product detail

--- resources/views/user/product-detail.blade.php
+++ resources/views/user/product-detail.blade.php
@@ -39,21 +39,23 @@
 
 
         </div>
+        <div class="container-fluid container-lg py-5 list-product">
+            <h2 class='text-left mb-3 font-weight-light'>Sản phẩm liên quan</h2>
+            <div class="row">
+                @for($i = 0; $i < 4; $i++) <div class="col-6 col-md-3 mb-3">
+                    <img class="w-100 rounded" src="{{ asset('https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a5f04850-c95a-4ed0-a2b3-1e6517c1278b/custom-nike-air-max-95-by-you.png') }}">
+                    <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limited</h5>
+                    <p class='mb-0 font-weight-bold text-danger'>1.200.000</p>
+                    <p class='mb-0 text-muted'>
+                        <del>2.929.000đ</del>
+                    </p>
+            </div>
+            @endfor
+        </div>
+
 
     </div>
-    <div class="container-fluid container-lg py-5 list-product">
-        <h2 class='text-left mb-3 font-weight-light'>Sản phẩm liên quan</h2>
-        <div class="row">
-            @for($i = 0; $i < 4; $i++) <div class="col-6 col-md-3 mb-3">
-                <img class="w-100 rounded" src="{{ asset('https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a5f04850-c95a-4ed0-a2b3-1e6517c1278b/custom-nike-air-max-95-by-you.png') }}">
-                <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limited</h5>
-                <p class='mb-0 font-weight-bold text-danger'>1.200.000</p>
-                <p class='mb-0 text-muted'>
-                    <del>2.929.000đ</del>
-                </p>
-        </div>
-        @endfor
-    </div>
+
 </div>
 </div>
 
