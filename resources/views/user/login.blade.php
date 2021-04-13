216b4b7e6c1aa9993d16da803908d45173093bd1
Bui Nhat Hao <bnhao10062001@gmail.com>
2021-04-13 17:01:33 +0700

    edit

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,34 +3,40 @@
 @section('title', 'Đăng nhập')
 
 @section('content')
-    <form class="py-5 form__login mx-auto" class="p-4">
-        <div class="form__tab row">
-            <div class="form__tab-item col-6 text-center">
-                <a class="nav-link d-block text-dark" href="{{ route('register') }}">Đăng ký</a>
-            </div>
-            <div class="form__tab-item col-6 text-center">
-                <a class="border-primary border-bottom form_tab-item-active nav-link d-block" href="#">Đăng nhập</a>
-            </div>
-        </div>
+    <div class="container-fluid py-5 bg-light">
+        <div class="row justify-content-center">
+            <form class="bg-white shadow-lg border my-5 form__login align-self-center">
+                <div class="form__tab row">
+                    <div class="form__tab-item col-6 text-center">
+                        <a class="nav-link d-block text-dark" href="{{ route('register') }}">Đăng ký</a>
+                    </div>
+                    <div class="form__tab-item col-6 text-center">
+                        <a class="border-primary border-bottom form_tab-item-active nav-link d-block" href="#">Đăng nhập</a>
+                    </div>
+                </div>
 
-        <div class="form-group">
-            <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
-        </div>
+                <div class='p-3'>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
+                    </div>
 
-        <div class="form-group">
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-                <label id="form-check-text" class="form-check-label" for="dropdownCheck2">
-                   Nhớ mật khẩu? 
-                </label>
-            </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                            <label id="form-check-text" class="form-check-label" for="dropdownCheck2">
+                            Nhớ mật khẩu? 
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" class=" form__login-btn btn btn-primary btn-block mx-auto btn">Đăng nhập</button>
+                    <div id='form__login-forget' class='border-light border-bottom text-center'>Quên mật khẩu?</div>
+                    <a href="{{ route('register') }}" id='form__login-btn-create' type="submit" class=" form__login-btn btn btn-light btn-block mx-auto">Tạo tài khoản mới</a>
+                </div>
+                
+            </form>
         </div>
-        <button type="submit" class=" form__login-btn btn btn-primary btn-block mx-auto btn">Đăng nhập</button>
-        <div id='form__login-forget' class='border-light border-bottom text-center'>Quên mật khẩu?</div>
-        <a href="{{ route('register') }}" id='form__login-btn-create' type="submit" class=" form__login-btn btn btn-light btn-block mx-auto">Tạo tài khoản mới</a>
-        
-    </form>
+    </div>
 @endsection
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -3,29 +3,37 @@
 @section('title', 'Đăng ký')
 
 @section('content')
-    <form class="py-5 form__login mx-auto" class="p-4">
-        <div class="form__tab row">
-            <div class=" form__tab-item col-6 text-center">
-                <a class=" border-primary text-primary border-bottom form_tab-item-active nav-link d-block" href="#">Đăng ký</a>
-            </div>
-            <div class="form__tab-item col-6 text-center">
-                <a class="nav-link d-block text-dark" href="{{ route('login') }}">Đăng nhập</a>
-            </div>
-        </div>
+    <div class="container-fluid py-5 bg-light">
+        <div class="row justify-content-center">
+            <form class="bg-white shadow-lg border my-5 form__login align-self-center">
+                <div class="form__tab row">
+                    <div class=" form__tab-item col-6 text-center">
+                        <a class=" border-primary text-primary border-bottom form_tab-item-active nav-link d-block" href="#">Đăng ký</a>
+                    </div>
+                    <div class="form__tab-item col-6 text-center">
+                        <a class="nav-link d-block text-dark" href="{{ route('login') }}">Đăng nhập</a>
+                    </div>
+                </div>
 
-        <div class="form-group">
-            <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Họ và tên của bạn ...">
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Số điện thoại ...">
-        </div>
-        <div class="form-group">
-            <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
-        </div>
+                <div class='p-3'>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Họ và tên của bạn ...">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="exampleDropdownFormPassword2" placeholder="Số điện thoại ...">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleDropdownFormPassword2" placeholder="Địa chỉ Email ...">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Mật khẩu ...">
+                    </div>
 
-        <button type="submit" class=" form__login-btn btn btn-primary btn-block mx-auto">Đăng ký</button>
-    </form>
+                    <button type="submit" class=" form__login-btn btn btn-primary btn-block mx-auto">Đăng ký</button>
+                    <p class="mt-3 text-center">Nếu có tài khoản, nhấn <a href=""> đăng nhập </a></p>
+                </div>
+
+            </form>
+        </div>
+    </div>
 @endsection
