c3ba4ce0626a71d4768d7a66ff7af740beb8e8ec
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-04-13 12:06:10 +0700

    add popper

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -17,5 +17,6 @@
 
     <script src="{{ asset('jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
