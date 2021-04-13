9d39eaac7803710c05ba8527e902473800435a9f
OaH <bnhao10062001@gmail.com>
2021-04-13 12:07:59 +0700

    edit-home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -29,7 +29,7 @@
         @for($i = 0; $i < 4; $i++)
             <div class="col-3">
                 <img class="w-100 rounded" src="{{ asset('https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/a5f04850-c95a-4ed0-a2b3-1e6517c1278b/custom-nike-air-max-95-by-you.png') }}">
-                <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limitet</h5>
+                <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limited</h5>
                 <p class='mb-0 font-weight-bold text-danger'>1.200.000</p>
                 <p class='mb-0 text-muted'>
                 <del>2.929.000đ</del>
@@ -48,7 +48,7 @@
         @for($i = 0; $i < 4; $i++)
             <div class="col-3">
                 <img class="w-100 rounded" src="{{ asset('https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4cdd870b-e576-4371-a21c-5295f7c6d222/air-max-97-se-shoe-tWNMCG.png') }}">
-                <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limitet</h5>
+                <h5 class='mt-2 font-weight-bold'>Nike AF1 mắc nhất thế giới limited</h5>
                 <p class='mb-0 font-weight-bold text-danger'>1.200.000</p>
                 <p class='mb-0 text-muted'>
                 <del>2.929.000đ</del>
