367a6f3418db232253584a316b176e324b34cb45
Son Bill <sonpewds@Asus-Zenbook-13-inch.local>
2021-04-13 19:44:16 +0700

    tao commit

--- resources/views/user/product-detail.blade.php
+++ resources/views/user/product-detail.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Chi tiet san pham')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row d-flex`">
-        <div class="col-md-12 col-sm-5">
-            <img src="https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/0f76f73e-2578-4d62-abab-c5563ea4f78c/dunk-low-black-release-date.jpg">
+<div class="container mt-2">
+    <div class="row">
+        <div class="col-md-6 d-flex mb-3">
+            <img class="w-100" src="https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/3b80f5a8-5819-4500-bb84-06cb1683863a/dunk-low-black-release-date.jpg">
         </div>
 
-        <div class="col-md-12 col-sm-5">
+        <div class="col-12 col-md-6 col-sm-12 mt-5">
             <h5>
                 Men's Trail Running Shoes
             </h5>
@@ -18,18 +18,30 @@
                 NIKE SB DUNK
             </h1>
 
-            <div class="d-flex">
-                <a class="">Chọn Size</a>
+            <label class="mt-3 col-md-6 col-sm-4 col-sm-3">Chọn Size</label>
 
-                <a class="">Size Guide</a>
 
+            <div class="btn-group btn-group-sm mt-1" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-light rounded-0 border">7 UK</button>
+                <button type="button" class="btn btn-light rounded-0 border">7.5 UK</button>
+                <button type="button" class="btn btn-light rounded-0 border">8 UK</button>
+                <button type="button" class="btn btn-light rounded-0 border">8.5 UK</button>
+                <button type="button" class="btn btn-light rounded-0 border">9 UK</button>
+                <button type="button" class="btn btn-light rounded-0 border">9.5 UK</button>
             </div>
 
-            <div class="d-flex flex-wrap">
-                <div><a>    </a></div> 
+            <div class="nut mt-5 col-md 6">
+                <button type="button" class="btn btn-lg btn-dark border rounded">Mua Ngay</button>
+                <button type="button" class="btn btn-lg border rounded">Thêm vào giỏ hàng</button>
             </div>
+
+            <p class="mt-5 text-justify">Không thể phủ nhận Nike Dunk là thiết kế bán chạy nhất của thương hiệu Swoosh và Nike Dunk Low City Market là biến thể mới nhất của giày thể thao cổ điển. Được các vận động viên bóng rổ, vận động viên trượt băng và sneakerhead cực kỳ yêu thích kể từ khi được giới thiệu vào năm 1985. </p>
+
 
         </div>
-    </div>
 
-    @endsection
+    </div>
+</div>
+</div>
+
+@endsection
