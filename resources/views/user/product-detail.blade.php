9570f1107dd642db491e03767b112fcefa6f2241
Son Bill <sonpewds@Asus-Zenbook-13-inch.local>
2021-04-17 23:16:16 +0700

    done product detail

--- resources/views/user/product-detail.blade.php
+++ resources/views/user/product-detail.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 <div class="container mt-2">
     <div class="row">
-        <div class="col-md-6 d-flex mb-3">
+        <div class="col-lg-6 d-flex">
             <img class="w-100" src="https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/3b80f5a8-5819-4500-bb84-06cb1683863a/dunk-low-black-release-date.jpg">
         </div>
 
-        <div class="col-12 col-md-6 col-sm-12 mt-5">
+        <div class="col-lg-6 mt-5">
             <h5>
                 Men's Trail Running Shoes
             </h5>
@@ -17,17 +17,23 @@
             <h1>
                 NIKE SB DUNK
             </h1>
+            <div>
+                <label class="mt-3">Chọn Size</label>
+            </div>
 
-            <label class="mt-3 d-flex">Chọn Size</label>
-
-
-            <div class="btn-group btn-group-md" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-light rounded-0 border">7 UK</button>
-                <button type="button" class="btn btn-light rounded-0 border">7.5 UK</button>
-                <button type="button" class="btn btn-light rounded-0 border">8 UK</button>
-                <button type="button" class="btn btn-light rounded-0 border">8.5 UK</button>
-                <button type="button" class="btn btn-light rounded-0 border">9 UK</button>
-                <button type="button" class="btn btn-light rounded-0 border">9.5 UK</button>
+            <div class="btn-group btn-group-md nut row" role="group" aria-label="Basic example">
+                <div class="col-12">
+                    <button type="button" class="btn btn-light rounded-0 border">7 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">7.5 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">8 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">8.5 UK</button>
+                </div>
+                <div class="col-12 mt-2">
+                    <button type="button" class="btn btn-light rounded-0 border">9 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">9.5 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">10 UK</button>
+                    <button type="button" class="btn btn-light rounded-0 border">10.5 UK</button>
+                </div>
             </div>
 
             <div class="mt-5">
